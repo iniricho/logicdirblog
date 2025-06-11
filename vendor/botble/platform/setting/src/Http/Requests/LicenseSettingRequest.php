9c44b0ec91befde34df9506d674d5a73dcd1d9ff
Richo Amanda Pratama <richo.amanda@detik.com>
2025-06-11 09:42:43 +0700

    Initial Commit

---
+++ vendor/botble/platform/setting/src/Http/Requests/LicenseSettingRequest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Botble\Setting\Http\Requests;
+
+use Botble\Support\Http\Requests\Request;
+
+class LicenseSettingRequest extends Request
+{
+    public function rules(): array
+    {
+        return [];
+    }
+}
